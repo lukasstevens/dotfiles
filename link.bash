1e9d70f66d3225b297e54fbcb46a45cb41fb0062
Lukas Stevens <mail@lukas-stevens.de>
2017-08-02 17:40:50 +0200

    Create linker script

---
+++ link.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# get the absolute path of this script
+pushd . > /dev/null
+script_path="${BASH_SOURCE[0]}";
+while([ -h "${script_path}" ]); do
+    cd "`dirname "${script_path}"`"
+    script_path="$(readlink "`basename "${script_path}"`")";
+done
+cd "`dirname "${script_path}"`" > /dev/null
+script_path="`pwd`";
+popd  > /dev/null
+
+link_names=(
+	"$HOME/.bashrc"
+	"$HOME/.emacs.d"
+	"$HOME/.config/i3"
+	"$HOME/.inputrc"
+	"$HOME/.vimrc"
+	"$HOME/.Xresources"
+	"$HOME/.xsessionrc"
+	"$HOME/.zshenv"
+	"$HOME/.zshrc")
+
+file_names=(
+	"bashrc"
+	"emacs.d"
+	"i3"
+	"inputrc"
+	"vimrc"
+	"Xresources"
+	"xsessionrc"
+	"zshenv"
+	"zshrc")
+
+
+for i in "${!file_names[@]}"; 
+do
+    rm ${link_names[$i]}
+    ln -s $script_path/${file_names[$i]} ${link_names[$i]}
+done
