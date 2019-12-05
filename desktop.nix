e7171a5a42ac7f2e01d020b027d809127cfeeb21
Lukas Stevens <mail@lukas-stevens.de>
2019-12-05 20:11:22 +0100

    Fix monitor order

--- desktop.nix
+++ desktop.nix
@@ -10,6 +10,6 @@
 
   xsession.initExtra = ''
 xrandr --output DP-2 --mode 3440x1440 --primary
-xrandr --output HDMI-4 --right-of DP-2 --auto
+xrandr --output HDMI-4 --left-of DP-2 --auto
     '';
 }
