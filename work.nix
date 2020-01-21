b99238d264cd6c9166355ed2af66130dce370e7e
Lukas Stevens <mail@lukas-stevens.de>
2020-01-21 10:44:24 +0100

    Fix monitor setting

--- work.nix
+++ work.nix
@@ -9,7 +9,7 @@
   };
 
   xsession.initExtra = ''
-xrandr --output DP1 --mode 2560x1440 --primary
-xrandr --output HDMI1 --right-of DP1 --mode 1920x1200
+xrandr --output DP1 --mode 2560x1440 --rate 59.95 --primary
+(xrandr --output HDMI1 --off && sleep 0.2s && xrandr --output HDMI1 --right-of DP1 --auto)
     '';
 }
