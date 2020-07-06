888e78f33ad2bf592056e224dbcc83210b2d2d40
Lukas Stevens <mail@lukas-stevens.de>
2020-07-06 21:42:07 +0200

    Use DP-3

--- desktop.nix
+++ desktop.nix
@@ -4,7 +4,7 @@
   imports = [ ~/dotfiles/home.nix ];
   
   xsession.initExtra = ''
-xrandr --output DP-2 --mode 3440x1440 --primary
+xrandr --output DP-3 --mode 3440x1440 --primary
 xrandr --output HDMI-4 --left-of DP-2 --auto
     '';
 }
