1215f7a5e7ffe198eaa40d5ab43111c737a5dbd9
Lukas Stevens <mail@lukas-stevens.de>
2020-07-18 20:02:32 +0200

    Add pulseeffects and new monitor

--- desktop.nix
+++ desktop.nix
@@ -4,7 +4,8 @@
   imports = [ ~/dotfiles/home.nix ];
   
   xsession.initExtra = ''
+pulseeffects --gapplication-service &
 xrandr --output DP-3 --mode 3440x1440 --primary
-xrandr --output HDMI-4 --left-of DP-2 --auto
+xrandr --output DVI-D-1 --left-of DP-3
     '';
 }
