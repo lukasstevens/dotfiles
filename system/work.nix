9fbc4934b8f2ed17c5645bfc7196e6927452ce5c
Lukas Stevens <mail@lukas-stevens.de>
2022-02-22 14:31:21 +0100

    Add work system config

--- system/work.nix
+++ system/work.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
 
@@ -8,4 +8,17 @@
 
   networking.hostName = "worknix";
 
+  services.timesyncd.servers = [
+    "ntp1.lrz.de"
+    "ntp2.lrz.de"
+    "ntp.lrz.de"
+    "ntp3.lrz.de"
+  ];
+
+  networking.firewall = {
+    allowedTCPPorts = [ 123 ];
+    allowedUDPPorts = [ 123 ];
+  };
+  
+  services.printing.drivers = [ pkgs.gutenprint pkgs.hplip ];
 }
