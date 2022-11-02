d339e08dafd19b1e0fc940a32d47ebbda795adb9
Lukas Stevens <mail@lukas-stevens.de>
2022-11-02 17:57:56 +0100

    Allow ports for AirPlay

--- system/xps.nix
+++ system/xps.nix
@@ -8,4 +8,12 @@
 
   networking.hostName = "nixps";
 
+  networking.firewall = {
+    allowedTCPPorts = [
+      7000 7100 # rpiplay
+    ];
+    allowedUDPPorts = [
+      6000 6001 7011 # rpiplay
+    ];
+  };
 }
