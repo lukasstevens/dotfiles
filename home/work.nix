5067aa9978fd3f7cd0ede993637c9d3c3e48dc20
Lukas Stevens <mail@lukas-stevens.de>
2022-11-03 13:44:36 +0100

    Configure monitors for work

--- home/work.nix
+++ home/work.nix
@@ -7,4 +7,9 @@
 xrandr --output DP1 --mode 2560x1440 --rate 59.95 --primary
 (xrandr --output HDMI1 --off && sleep 0.2s && xrandr --output HDMI1 --right-of DP1 --auto)
     '';
+
+  wayland.windowManager.sway.config.output = {
+    "DP-1" = { pos = "0 0"; };
+    "HDMI-A-1" = { pos = "2560 0"; };
+  };
 }
