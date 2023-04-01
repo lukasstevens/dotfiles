ef9678d4a3a248def5e8fee00c27a357e2036d6e
Lukas Stevens <mail@lukas-stevens.de>
2023-04-01 20:15:35 +0200

    Use rofi instead of wofi

--- nix/base16-rofi-template/default.nix
+++ nix/base16-rofi-template/default.nix
@@ -4,12 +4,7 @@ stdenv.mkDerivation {
   pname = "base16-rofi-template";
   version = "for-mybase16-theme";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "tinted-theming";
-    repo = "base16-rofi";
-    rev = "3f64a9f8d8cb7db796557b516682b255172c4ab4";
-    sha256 = "03y4ydnd6sijscrrp4qdvckrckscd39r8gyhpzffs60a1w4n76j5";
-  };
+  src = ./.;
 
   installPhase = ''
     mkdir -p $out
