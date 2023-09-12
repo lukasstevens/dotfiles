2dba9b42b8da3cd8308703f45125dc5c1d79406d
Lukas Stevens <mail@lukas-stevens.de>
2023-09-12 20:12:48 +0200

    Enable virtualisation

--- system/desktop.nix
+++ system/desktop.nix
@@ -13,4 +13,8 @@
   networking.firewall.allowedUDPPorts = [
     8912 8930 # IsarLight
   ];
+
+  virtualisation.libvirtd.enable = true;
+  users.extraUsers."lukas".extraGroups = [ "libvirtd" ];
+  networking.firewall.checkReversePath = true;
 }
