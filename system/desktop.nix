02d91f3fda31dbae56e91defd9ad850d6f44bfeb
Lukas Stevens <mail@lukas-stevens.de>
2023-09-27 19:59:08 +0200

    Unbind Behringer interface before suspend

--- system/desktop.nix
+++ system/desktop.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
 
@@ -17,4 +17,14 @@
   virtualisation.libvirtd.enable = true;
   users.extraUsers."lukas".extraGroups = [ "libvirtd" ];
   networking.firewall.checkReversePath = true;
+
+  powerManagement.powerDownCommands =
+    let
+      behringer-id = "1397:0508";
+    in
+    # The Behringer interface prevents suspending so we unbind it in advance.
+    ''
+    bus_path=$(${pkgs.usbutils}/bin/lsusb -tvv | ${pkgs.gawk}/bin/awk '/${behringer-id}/ { getline; print gensub(/.*devices\/([0-9\-\.]*)/, "\\1", "g", $1); exit; }')
+    echo "$bus_path" > /sys/bus/usb/drivers/usb/unbind
+    '';
 }
