ab561965ef8d47975d208a698d6844a09368983e
Lukas Stevens <mail@lukas-stevens.de>
2023-12-11 11:25:15 +0100

    Option was renamed

--- home/home.nix
+++ home/home.nix
@@ -205,7 +205,7 @@ in {
 
   wayland.windowManager.sway = {
     enable = true;
-    systemdIntegration = true;
+    systemd.enable = true;
     xwayland = true;
     wrapperFeatures.gtk = true;
     extraSessionCommands = ''
